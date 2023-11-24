815106186b7c4042d6cbdf2d585f515b48329948
Sunny Lin <iluvsunshinezduqz@gmail.com>
2023-11-24 14:21:31 -0800

    yjd

--- Functions.py
+++ Functions.py
@@ -2,18 +2,18 @@
 # Sunny Lin
 # Nov 24, 23
 
-# def area_of_a_square(sidelength:float):
-# 	'''
-# 	Calculate and print the area of a square.
-# 	Results are in units squared.
+def print_area_of_a_square(sidelength:float):
+	'''
+	Calculate and print the area of a square.
+	Results are in units squared.
 	
-# 	Params:
-# 	• sidelength = length of one side of the square.
-# 	'''
+	Params:
+	• sidelength = length of one side of the square.
+	'''
 	
-# 	area=sidelength**2
+	area=sidelength**2
 	
-#   print(f'The area of a square with side length {sidelength} is {area} square units.')
+	print(f'The area of a square with side length {sidelength} is {area} square units.')
 
 def area_of_a_square(sidelength:float)->float:
 	'''
@@ -27,6 +27,9 @@ def area_of_a_square(sidelength:float)->float:
 	area=sidelength**2
 	
 	return area
+
+print(print_area_of_a_square(12.2))
+print(print_area_of_a_square(12))
 
 print(area_of_a_square(12.2))
 print(area_of_a_square(12))
