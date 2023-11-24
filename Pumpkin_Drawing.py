dfc9f9afb0438ec020adb4c648f4ecce968ba35d
Sunny Lin <iluvsunshinezduqz@gmail.com>
2023-11-24 15:01:55 -0800

    dfh

--- Pumpkin_Drawing.py
+++ Pumpkin_Drawing.py
@@ -34,11 +34,35 @@ screen.bgcolor("black")
 # screen.mainloop()
 
 turtle.speed(0)
+
+# while True:
+#     turtle.color(random.random(),random.random(),random.random())
+#     turtle.pu()
+#     turtle.goto(random.randint(-screen.window_width()//2,screen.window_width()//2),random.randint(-screen.window_height()//2,screen.window_height()//2))
+#     turtle.right(random.randint(1,360))
+#     turtle.pensize(random.randint(1,100))
+#     turtle.pd()
+#     turtle.circle(random.randint(1,500))
+
+# turtle.color('white')
+# num=1
+# direction1=0
+# direction2=0
+# while True:
+#     turtle.forward(num)
+#     turtle.pu()
+#     turtle.backward(num)
+#     direction1=(direction1+num)%360
+#     turtle.seth(direction1)
+#     turtle.pd()
+#     turtle.forward(num)
+#     turtle.seth(direction1)
+#     num+=1
+#     direction2+=1
+
+turtle.color('white')
+num=1
 while True:
-    turtle.color(random.random(),random.random(),random.random())
-    turtle.pu()
-    turtle.goto(random.randint(-screen.window_width()//2,screen.window_width()//2),random.randint(-screen.window_height()//2,screen.window_height()//2))
-    turtle.right(random.randint(1,360))
-    turtle.pensize(random.randint(1,100))
-    turtle.pd()
-    turtle.circle(random.randint(1,500))
+    turtle.forward(50)
+    turtle.left(num)
+    num+=1
