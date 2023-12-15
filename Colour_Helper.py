bf5fb1041073a59266b113de255877a7c49a6464
Sunny Lin <iluvsunshinezduqz@gmail.com>
2023-12-15 14:21:30 -0800

    hd

--- Colour_Helper.py
+++ Colour_Helper.py
@@ -2,9 +2,7 @@
 # Sunny Lin
 # Dec 13, 23
 
-from PIL import Image
-
-def pixel_to_string(pixel:tuple)->str:
+def is_green(pixel:tuple)->str:
     '''
     Return if a pixel is green.
 
@@ -12,4 +10,4 @@ def pixel_to_string(pixel:tuple)->str:
     • pixel:tuple = the pixel to be checked.
     '''
 
-    return pixel[0]<32 and pixel[1]>250 and pixel[2]<32
+    return pixel[0]<120 and pixel[1]>180 and pixel[2]<120
