0fc6fcc1cf026bdcc4cdb319ff23457ea1346367
Sunny Lin <iluvsunshinezduqz@gmail.com>
2023-12-15 13:54:38 -0800

    afh

--- Images_Example.py
+++ Images_Example.py
@@ -6,13 +6,16 @@ from PIL import Image
 
 import Colour_Helper
 
-with Image.open("/Users/sl000268/Programming/SLSS-Programming-23-24/Images/kid-green.jpg") as im:
-    # Outer loop is top->bottom.
-    for y in range(im.height):
-        # Inner loop is left->right.
-        for x in range(im.width):
-            # Detects if the current pixel is green.
-            if Colour_Helper.pixel_to_string(im.getpixel((x,y))):
-                print("GREEN PIXEL!!!!")
-            else:
-                print("UNKNOWN PIXEL")
+with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/kid-green.jpg') as subject:
+    with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/beach.jpg') as background:
+        # Outer loop is top->bottom.
+        for y in range(subject.height):
+            # Inner loop is left->right.
+            for x in range(subject.width):
+                # if the current pixel is green:
+                if Colour_Helper.pixel_to_string(subject.getpixel((x,y))):
+                    # Switch the current pixel with the corresponding background pixel.
+                    subject.putpixel((x,y),background.getpixel((x,y)))
+        
+        # Save the image.
+        subject.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/output.jpg')
