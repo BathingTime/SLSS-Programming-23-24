4efd7cc95f4f3abc966be4fe196dfd0431fe4344
Sunny Lin <iluvsunshinezduqz@gmail.com>
2023-12-20 11:32:11 -0800

    gf

--- Images_Example.py
+++ Images_Example.py
@@ -7,121 +7,95 @@ from random import randint
 
 import Colour_Helper
 
-# Greenscreen
+def greenscreen(subject_name:str,background_name:str)->None:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{subject_name}.jpg') as subject:
+        with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{background_name}.jpg') as background:
+            for y in range(subject.height):
+                for x in range(subject.width):
+                    if Colour_Helper.is_green(subject.getpixel((x,y))):
+                        subject.putpixel((x,y),background.getpixel((x,y)))
+            subject.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/greenscreen.jpg')
 
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/kid-green.jpg') as subject:
-#     with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/beach.jpg') as background:
-#         # Iterate through each row of pixel in the picture.
-#         for y in range(subject.height):
-#             # Iterate through each pixel of each row.
-#             for x in range(subject.width):
-#                 # If the current pixel is green, switch it with its corresponding background pixel.
-#                 if Colour_Helper.is_green(subject.getpixel((x,y))):
-#                     subject.putpixel((x,y),background.getpixel((x,y)))
+def binary(im_name:str)->None:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                if Colour_Helper.is_light(im.getpixel((x,y))):
+                    im.putpixel((x,y),(255,255,255))
+                else:
+                    im.putpixel((x,y),(0,0,0))
+        im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/binary.jpg')
 
-#         # Save the image.
-#         subject.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/greenscreen_example.jpg')
+def grayscale(im_name:str)->None:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                new_rgb=Colour_Helper.gray_scale(im.getpixel((x,y)))
+                im.putpixel((x,y),tuple([new_rgb]*3))
+        im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/grayscale.jpg')
 
-# Binary
+def contrary(im_name:str)->None:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                im.putpixel((x,y),Colour_Helper.contrary(im.getpixel((x,y))))
+    im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/contrary.jpg')
 
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-#     # Iterate through each row of pixel in the picture.
-#     for y in range(im.height):
-#         # Iterate through each pixel of each row.
-#         for x in range(im.width):
-#             # Change the current pixel to white if it is light or black if it is dark.
-#             if Colour_Helper.is_light(im.getpixel((x,y))):
-#                 im.putpixel((x,y),(255,255,255))
-#             else:
-#                 im.putpixel((x,y),(0,0,0))
+def random(im_name:str)->None:
+    new_rgb={}
+    for current in range(0,256):
+        new_rgb[current]=randint(0,255)
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                im.putpixel((x,y),tuple(new_rgb[rgb] for rgb in im.getpixel((x,y))))
+        im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random.jpg')
 
-#     # Save the image.
-#     im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/binary_example.jpg')
-
-# Gray-scale
-
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-#     # Iterate through each row of pixel in the picture.
-#     for y in range(im.height):
-#         # Iterate through each pixel of each row.
-#         for x in range(im.width):
-#             # Change the current pixel to its gray-scaled version.
-#             new_rgb=Colour_Helper.gray_scale(im.getpixel((x,y)))
-#             im.putpixel((x,y),tuple([new_rgb]*3))
-
-#     # Save the image.
-#     im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/grayscaled_example.jpg')
-
-# Contrary
-
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-#     # Iterate through each row of pixel in the picture.
-#     for y in range(im.height):
-#         # Iterate through each pixel of each row.
-#         for x in range(im.width):
-#             # Change the current pixel to its contrary version.
-#             im.putpixel((x,y),Colour_Helper.contrary(im.getpixel((x,y))))
-
-#     # Save the image.
-#     im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/contrary_example.jpg')
-
-# Random
-
-# new_rgb={}
-
-# for current in range(0,256):
-#     new_rgb[current]=randint(0,255)
-
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-#     # Iterate through each row of pixel in the picture.
-#     for y in range(im.height):
-#         # Iterate through each pixel of each row.
-#         for x in range(im.width):
-#             # Change the current pixel to its contrary version.
-#             im.putpixel((x,y),tuple(new_rgb[rgb] for rgb in im.getpixel((x,y))))
-
-#     # Save the image.
-#     im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random_example.jpg')
-
-# Random 2
-
-# new_rgb={}
-
-# for current in range(0,255//10+1):
-#     new_rgb[current]=randint(0,255)
-
-# with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-#     # Iterate through each row of pixel in the picture.
-#     for y in range(im.height):
-#         # Iterate through each pixel of each row.
-#         for x in range(im.width):
-#             # Change the current pixel to its contrary version.
-#             im.putpixel((x,y),tuple(new_rgb[rgb//10] for rgb in im.getpixel((x,y))))
-
-#     # Save the image.
-#     im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random2_example.jpg')
-
-# Random 3
-
-new_rgbs=[]
-
-for _ in range(3):
+def random2(im_name:str)->None:
     new_rgb={}
     for current in range(0,255//10+1):
         new_rgb[current]=randint(0,255)
-    new_rgbs.append(new_rgb)
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                im.putpixel((x,y),tuple(new_rgb[rgb//10] for rgb in im.getpixel((x,y))))
+        im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random2.jpg')
 
-with Image.open('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/best_pizza.jpg') as im:
-    # Iterate through each row of pixel in the picture.
-    for y in range(im.height):
-        # Iterate through each pixel of each row.
-        for x in range(im.width):
-            # Change the current pixel to its contrary version.
-            current=im.getpixel((x,y))
-            random_rgb=[]
-            for pos_rgb in range(3):
-                random_rgb.append(new_rgbs[pos_rgb][current[pos_rgb]//10])
-            im.putpixel((x,y),tuple(random_rgb))
+def random3(im_name:str)->None:
+    new_rgbs=[]
+    for _ in range(3):
+        new_rgb={}
+        for current in range(0,255//10+1):
+            new_rgb[current]=randint(0,255)
+        new_rgbs.append(new_rgb)
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                current=im.getpixel((x,y))
+                random_rgb=[]
+                for pos_rgb in range(3):
+                    random_rgb.append(new_rgbs[pos_rgb][current[pos_rgb]//10])
+                im.putpixel((x,y),tuple(random_rgb))
+        im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random3.jpg')
 
-    # Save the image.
-    im.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/random3_example.jpg')
+def scramble(im_name:str)->None:
+    rows,cols=100,1
+    grid=[[(x,y) for x in range(cols)] for y in range(rows)]
+    for y in range(rows):
+        for x in range(cols):
+            new_pos=(randint(0,rows-1),randint(0,cols-1))
+            grid[y][x],grid[new_pos[0]][new_pos[1]]=grid[new_pos[0]][new_pos[1]],grid[y][x]
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im1:
+        width,height=im1.width//cols,im1.height//rows
+        with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im2:
+            for grid_y in range(rows):
+                for grid_x in range(cols):
+                    for im_y in range(height):
+                        for im_x in range(width):
+                            im2.putpixel((grid[grid_y][grid_x][0]*width+im_x,grid[grid_y][grid_x][1]*height+im_y),im1.getpixel((grid_x*width+im_x,grid_y*height+im_y)))
+            im2.save('/Users/sl000268/Programming/SLSS-Programming-23-24/Images/scramble.jpg')
+
+def wiggle(im_name:str)->None:
+    ...
+
+wiggle('best_pizza')
