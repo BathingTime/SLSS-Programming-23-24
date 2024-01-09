7c65182338fd4282f8df7da5f4aa3cf3c32e2874
Sunny Lin <iluvsunshinezduqz@gmail.com>
2024-01-09 09:50:28 -0800

    eg

--- Bean_Counter.py
+++ Bean_Counter.py
@@ -2,3 +2,19 @@
 # Sunny Lin
 # Jan 9, 24
 
+from PIL import Image
+
+import Bean_Helper
+
+def count_colours(im_name:str)->None:
+    colours={('red',Bean_Helper.red):0,
+             ('orange',Bean_Helper.orange):0,
+             ('yellow',Bean_Helper.yellow):0,
+             ('green',Bean_Helper.green):0,
+             ('blue',Bean_Helper.blue):0,
+             ('pink',Bean_Helper.pink):0,
+             ('black',Bean_Helper.black):0,}
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpg') as im:
+        for y in range(im.height):
+            for x in range(im.width):
+                for 
