2363b2321e2abbcc32e57a060a8abf7977d876b6
Sunny Lin <iluvsunshinezduqz@gmail.com>
2024-01-12 14:38:25 -0800

    dhs

--- Bean_Helper.py
+++ Bean_Helper.py
@@ -6,7 +6,7 @@ def red(pixel:tuple)->bool:
     return 100<=pixel[0] and 0<=pixel[1]<=10 and 0<=pixel[2]<=20
 
 def orange(pixel:tuple)->bool:
-    return 180<=pixel[0] and 50<=pixel[1]<=170 and 0<=pixel[2]<=30
+    return 200<=pixel[0] and 50<=pixel[1]<=150 and 0<=pixel[2]<=30
 
 def yellow(pixel:tuple)->bool:
     return 200<=pixel[0] and 150<=pixel[1]<=210 and 0<=pixel[2]<=30
@@ -15,7 +15,7 @@ def green(pixel:tuple)->bool:
     return 10<=pixel[0]<=80 and 50<=pixel[1] and 0<=pixel[2]<=60
 
 def blue(pixel:tuple)->bool:
-    return 0<=pixel[0]<=110 and 20<=pixel[1]<=100 and 110<=pixel[2]
+    return 0<=pixel[0]<=110 and 20<=pixel[1]<=100 and 80<=pixel[2]
 
 def pink(pixel:tuple)->bool:
     return 190<=pixel[0]<=240 and 60<=pixel[1]<=150 and 80<=pixel[2]<=200
---
+++ Robot_Dog.py
@@ -0,0 +1,26 @@
+# Robot Dog
+# Sunny Lin
+# Jan 12, 24
+
+from PIL import Image
+
+def is_red(pixel:tuple)->bool:
+    return 170<=pixel[0] and 10<=pixel[1]<=80 and 20<=pixel[2]<=100
+
+def ball_centre(im_name:str)->str:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpeg') as im:
+        left,right,top,bottom=im.width,0,im.height,0
+        for y in range(im.height):
+            for x in range(im.width):
+                if is_red(im.getpixel((x,y))):
+                    if x<left:
+                        left=x
+                    if right<x:
+                        right=x
+                    if y<top:
+                        top=y
+                    if bottom<y:
+                        bottom=y
+        return f'The centre of the ball is at ({(left+right)//2}, {(top+bottom)//2}) from the top-right.'
+
+print(ball_centre('Red_Ball'))
