9f3ffa31dbccef97348b65f86d4283d03130b3f8
Sunny Lin <iluvsunshinezduqz@gmail.com>
2024-01-15 13:55:40 -0800

    dsfh

--- Robot_Dog.py
+++ Robot_Dog.py
@@ -8,7 +8,7 @@ def is_red(pixel:tuple)->bool:
     return 170<=pixel[0] and 10<=pixel[1]<=80 and 20<=pixel[2]<=100
 
 def ball_centre(im_name:str)->str:
-    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}.jpeg') as im:
+    with Image.open(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/{im_name}') as im:
         left,right,top,bottom=im.width,0,im.height,0
         for y in range(im.height):
             for x in range(im.width):
@@ -21,6 +21,10 @@ def ball_centre(im_name:str)->str:
                         top=y
                     if bottom<y:
                         bottom=y
+        for y in range((top+bottom)//2-1,(top+bottom)//2+1):
+            for x in range((left+right)//2-1,(left+right)//2+1):
+                im.putpixel((x,y),(0,0,0))
+        im.save(f'/Users/sl000268/Programming/SLSS-Programming-23-24/Images/Ball_Centre.jpg')
         return f'The centre of the ball is at ({(left+right)//2}, {(top+bottom)//2}) from the top-right.'
 
-print(ball_centre('Red_Ball'))
+print(ball_centre('Red_Ball.jpeg'))
