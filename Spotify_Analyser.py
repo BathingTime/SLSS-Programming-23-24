ceb0d8c3d0b42ea9c8a328f3433f1caf9d30efa5
Sunny Lin <iluvsunshinezduqz@gmail.com>
2024-01-19 14:39:41 -0800

    mhv

--- Spotify_Analyser.py
+++ Spotify_Analyser.py
@@ -23,7 +23,7 @@ with open('/Users/sl000268/Programming/SLSS-Programming-23-24/spotify.csv') as d
         if line_num==0:
             print(f'The columns are: {", ".join(line)}')
         else:
-            # If the artist is Drake, append the artist, song name, and danceability as a tuple into the Drake list.:
+            # If the artist is Drake, append the artist, song name, valence, and danceability as a tuple into the Drake list:
             if 'drake' in line['artist'].lower():
                 drake_songs.append((line['artist'],line['song_title'],line['valence'],line['danceability']))
         
@@ -31,6 +31,14 @@ with open('/Users/sl000268/Programming/SLSS-Programming-23-24/spotify.csv') as d
         line_num+=1
 
 # Print all the Drake songs that have danceability of .5 or higher.
-for song in drake_songs:
-    if float(song[3])>=.5:
-        print(song[1])
+# for song in drake_songs:
+#     if float(song[3])>=.5:
+#         print(song[1])
+
+# Sort using selection sort from smallest to biggest danceability.
+for pos1 in range(len(drake_songs)-1):
+    pos_smallest=pos1
+    for pos2 in range(pos1+1,len(drake_songs)):
+        if drake_songs[pos2][3]<drake_songs[pos_smallest][3]:
+            pos_smallest=pos2
+    drake_songs[pos1],drake_songs[pos_smallest]=drake_songs[pos_smallest],drake_songs[pos1]
